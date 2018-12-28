2709a02b7882b0ea3efaa854259510067f0bb14c
joseramirez013 <joseramirez013@hotmail.com>
2018-12-27 21:09:30 -0600

    Update app.js

---
+++ codBase/js/app.js
@@ -0,0 +1,25 @@
+//Declaración de variables
+var operando_1;
+var operando_2;
+var operacion;
+
+function init(){
+  //Variables
+  var resultado = document.getElementById('igual');
+  var reset = document.getElementById('on');
+  var suma = document.getElementById('mas');
+  var resta = document.getElementById('menos');
+  var multiplicacion = document.getElementById('por');
+  var division = document.getElementById('dividido');
+  var uno = document.getElementById('1');
+  var dos = document.getElementById('2');
+  var tres = document.getElementById('3');
+  var cuatro = document.getElementById('4');
+  var cinco = document.getElementById('5');
+  var seis = document.getElementById('6');
+  var siete = document.getElementById('7');
+  var ocho = document.getElementById('8');
+  var nueve = document.getElementById('9');
+  var cero = document.getElementById('0');
+  var punto = document.getElementById('punto')
+}
