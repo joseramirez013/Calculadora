c54997a35257c514909dce4e7899b49293be1b68
joseramirez013 <joseramirez013@hotmail.com>
2018-12-27 22:13:14 -0600

    Cargar funcion Init en el body.

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -3,9 +3,10 @@ var operando_1;
 var operando_2;
 var operacion;
 
+
 function init(){
   //Variables
-  var resultado = document.getElementById('display');
+  var display = document.getElementById('display');
   var reset = document.getElementById('on');
   var suma = document.getElementById('mas');
   var resta = document.getElementById('menos');
@@ -22,4 +23,37 @@ function init(){
   var nueve = document.getElementById('9');
   var cero = document.getElementById('0');
   var punto = document.getElementById('punto')
+
+
+  //Eventos de click
+  uno.onclick = function(event){
+    display.textContent = display.textContent + "1";
+  }
+  dos.onclick = function(event){
+    display.textContent = display.textContent + "2";
+  }
+  tres.onclick = function(event){
+    display.textContent = display.textContent + "3";
+  }
+  cuatro.onclick = function(event){
+    display.textContent = display.textContent + "4";
+  }
+  cinco.onclick = function(event){
+    display.textContent = display.textContent + "5";
+  }
+  seis.onclick = function(event){
+    display.textContent = display.textContent + "6";
+  }
+  siete.onclick = function(event){
+    display.textContent = display.textContent + "7";
+  }
+  ocho.onclick = function(event){
+    display.textContent = display.textContent + "8";
+  }
+  nueve.onclick = function(event){
+    display.textContent = display.textContent + "9";
+  }
+  cero.onclick = function(event){
+    display.textContent = display.textContent + "0";
+  }
 }
